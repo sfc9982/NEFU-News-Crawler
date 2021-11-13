28b7153caaed5b75883427a4b473ff521b1b3497
sfc9982 <chnmc_yan_xin@outlook.com>
2021-11-13 16:15:14 +0800

    Get Title

--- nefuSpider/nefuSpider/spiders/icec.py
+++ nefuSpider/nefuSpider/spiders/icec.py
@@ -7,5 +7,9 @@ class IcecSpider(scrapy.Spider):
     start_urls = ['https://icec.nefu.edu.cn/index/xwzx.htm']
 
     def parse(self, response):
-        filename = "xwzx.htm"
-        open(filename, 'wb+').write(response.body)  # need to be wb+, or treated as str -> fail
+        # filename = "xwzx.htm"
+        # open(filename, 'wb+').write(response.body)  # need to be wb+, or treated as str -> fail
+        context = response.xpath('//*[@id="line_u8_4"]/a/@title')
+        title_present = context.extract_first()
+        print(title_present)
+        pass
