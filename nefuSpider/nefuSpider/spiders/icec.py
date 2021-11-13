55a2b15b3945efe9304b5d62037180be75b4bded
sfc9982 <chnmc_yan_xin@outlook.com>
2021-11-13 15:51:46 +0800

    unavailable now

--- nefuSpider/nefuSpider/spiders/icec.py
+++ nefuSpider/nefuSpider/spiders/icec.py
@@ -3,8 +3,9 @@ import scrapy
 
 class IcecSpider(scrapy.Spider):
     name = 'icec'
-    allowed_domains = ['https://icec.nefu.edu.cn/']
-    start_urls = ['http://https://icec.nefu.edu.cn//']
+    allowed_domains = ['icec.nefu.edu.cn']
+    start_urls = ['https://icec.nefu.edu.cn/index/xwzx.htm']
 
     def parse(self, response):
-        pass
+        filename = "xwzx.htm"
+        open(filename, 'w').write(response.body)
