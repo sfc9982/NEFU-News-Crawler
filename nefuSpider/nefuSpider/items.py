5a0659f6c7097bd89e829dc9872bc40564e856e4
sfc9982 <chnmc_yan_xin@outlook.com>
2021-11-14 09:17:18 +0800

    New Class for News

--- nefuSpider/nefuSpider/items.py
+++ nefuSpider/nefuSpider/items.py
@@ -9,4 +9,7 @@ import scrapy
 class NefuspiderItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
-    pass
+    name = scrapy.Field()
+    title = scrapy.Field()
+    para = scrapy.Field()
+    # pass
--- nefuSpider/nefuSpider/spiders/icec.py
+++ nefuSpider/nefuSpider/spiders/icec.py
@@ -1,4 +1,5 @@
 import scrapy
+from nefuSpider.nefuSpider.items import NefuspiderItem
 
 
 class IcecSpider(scrapy.Spider):
@@ -9,7 +10,27 @@ class IcecSpider(scrapy.Spider):
     def parse(self, response):
         # filename = "xwzx.htm"
         # open(filename, 'wb+').write(response.body)  # need to be wb+, or treated as str -> fail
-        context = response.xpath('//*[@id="line_u8_4"]/a/@title')
-        title_present = context.extract_first()
-        print(title_present)
-        pass
+        # context = response.xpath('//*[@id]/a/@title')
+        # title_present = context.extract_first()
+        # print(title_present)
+        # 存放新闻信息的集合
+        items = []
+
+        for each in response.xpath("//*[@id]/a/@title"):
+            # 将我们得到的数据封装到一个 `ItcastItem` 对象
+            item = NefuspiderItem()
+            # extract()方法返回的都是unicode字符串
+            name = each.xpath("h3/text()").extract()
+            title = each.xpath("h4/text()").extract()
+            para = each.xpath("p/text()").extract()
+
+            # xpath返回的是包含一个元素的列表
+            item['name'] = name[0]
+            item['title'] = title[0]
+            item['para'] = para[0]
+
+            items.append(item)
+
+        # 直接返回最后数据
+        return items
+        # pass
